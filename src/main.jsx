bce46977356c038b93b61a0a5431c08ce2774f84
killuaZolyck69 <tanver694225@gmail.com>
2025-07-26 23:18:26 +0600

    fixes some error

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/AppRoutes";
+import AuthProvider from "./providers/AuthProvider";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster />
+      </QueryClientProvider>
+    </AuthProvider>
+  </StrictMode>
+);
